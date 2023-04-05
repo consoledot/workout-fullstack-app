3e9ad7cdb8ce8806f78b43238ecc863ee11808a6
Abimbola Oladele <bimbooladele@outlook.com>
2023-04-05 23:27:09 +0100

    added react router, component and fix cors issues

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const workoutRoutes = require("./route/workout");
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 app.use(express.json());
 app.use((req, res, next) => {
   console.log(req.path, req.method);
